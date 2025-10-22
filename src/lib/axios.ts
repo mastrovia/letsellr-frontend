309146302878e5f05049a40cbd26250995abe403
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-10-22 23:40:26 +0530

    feat: update env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.NEXT_PUBLIC_BACKEND_BASE_URL || "http://localhost:4500/letseller",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4500/letseller",
   withCredentials: true,
 });
 export default instance;
