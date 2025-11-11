2228f464d31eda9afe9e75269989724dc6b1ef04
Remin T Roy <86014784+remintroy@users.noreply.github.com>
2025-11-11 10:40:40 +0530

    feat: added posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+);
